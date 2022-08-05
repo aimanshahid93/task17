8dd502383133bc162e7c6e82c550eaacdbddf7c1
aimanshahid93 <108936343+aimanshahid93@users.noreply.github.com>
2022-08-05 16:44:57 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,36 @@
+const countdown=document.querySelector(".countdown");
+let time=10;
+countdown.innerText=time--;
+setTimeout(() => {
+    countdown.innerText=time--;
+    setTimeout(() => {
+        countdown.innerText=time--;
+        setTimeout(() => {
+            countdown.innerText=time--;
+    setTimeout(() => {
+        countdown.innerText=time--;
+        setTimeout(() => {
+            countdown.innerText=time--;
+            setTimeout(() => {
+                countdown.innerText=time--;
+            setTimeout(() => {
+             countdown.innerText=time--;
+             setTimeout(() => {
+                countdown.innerText=time--;
+        setTimeout(() => {
+            countdown.innerText=time--;
+        },1000);
+        setTimeout(() => {
+            countdown.innerText=time--;
+            setTimeout(() => {
+                countdown.innerText="Happy Independance Day"
+            }, 1000);
+        },1000);
+    },1000)
+}, 1000);
+},1000);
+},1000);
+},1000);
+},1000);
+},1000);
+},1000);
